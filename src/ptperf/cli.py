7a2b7ed25b42266a3dc06a77b31d0915907f4173
mart-mihkel <mart.mihkel.aun@gmail.com>
2026-04-04 08:52:57 +0300

    training args

--- src/ptperf/cli.py
+++ src/ptperf/cli.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections.abc import Callable
 from typing import Annotated, Literal
 
 from typer import Option, Typer
@@ -10,39 +9,29 @@ from ptperf.types import Task
 app = Typer(no_args_is_help=True)
 
 
-def timed(func: Callable) -> Callable:
-    import time
-    from functools import wraps
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        from ptperf.logging import logger
-
-        start = time.time()
-        result = func(*args, **kwargs)
-        elapsed = time.time() - start
-
-        hours, remainder = divmod(int(elapsed), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        logger.info("time elapsed %02d:%02d:%02d", hours, minutes, seconds)
-
-        return result
-
-    return wrapper
-
-
-@app.command()
-@timed
+@app.command(no_args_is_help=True)
 def main(
     model_path: Annotated[str, Option()],
     task: Annotated[Task.__value__, Option()],
+    epochs: Annotated[int, Option()] = 3,
+    batch_size: Annotated[int, Option()] = 8,
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO",
-):
+) -> None:
+    import time
+
     from ptperf.logging import logger
     from ptperf.scripts import fine_tune
 
     logger.setLevel(log_level)
-    fine_tune(model_path, task)
+
+    start = time.time()
+    run_name = "notimplemented"
+    fine_tune(model_path, task, run_name, epochs, batch_size)
+    elapsed = time.time() - start
+
+    hours, remainder = divmod(int(elapsed), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    logger.info("time elapsed %02d:%02d:%02d", hours, minutes, seconds)
 
 
 if __name__ == "__main__":
--- src/ptperf/scripts.py
+++ src/ptperf/scripts.py
@@ -1,3 +1,4 @@
+import os
 from typing import cast
 
 import torch
@@ -67,26 +68,35 @@ def _load_collator(tokenizer: PreTrainedTokenizerFast, task: Task) -> DataCollat
     raise NotImplementedError(f"Collator for task: {task}")
 
 
-def _get_training_args() -> TrainingArguments:
+def _get_training_args(
+    run_name: str,
+    epochs: int,
+    batch_size: int,
+) -> TrainingArguments:
     have_cuda = torch.cuda.is_available()
+    logdir = os.path.join("log", run_name)
 
     if have_cuda:
         logger.debug("have accelerator")
+        logger.debug("using brain floating point 16")
         optim = "adamw_8bit"
     if not have_cuda:
         logger.warning("no accelerator")
+        logger.debug("using full precision floating point")
         optim = "adamw_torch_fused"
 
+    logger.debug("logdir %s", logdir)
     logger.debug("using %s optimizer", optim)
 
     return TrainingArguments(
-        output_dir="out/test",
+        run_name=run_name,
+        output_dir=logdir,
         save_strategy="no",
         eval_strategy="epoch",
         logging_steps=500,
-        num_train_epochs=5,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         use_cpu=not have_cuda,
         bf16_full_eval=have_cuda,
         bf16=have_cuda,
@@ -94,14 +104,20 @@ def _get_training_args() -> TrainingArguments:
     )
 
 
-def fine_tune(model_path: str, task: Task):
+def fine_tune(
+    model_path: str,
+    task: Task,
+    run_name: str,
+    epochs: int,
+    batch_size: int,
+) -> None:
     logger.debug("load %s config", model_path)
     config = AutoConfig.from_pretrained(model_path)
     tokenizer = _load_tokenizer(model_path)
     data = load_data(tokenizer, config, task)
     model = _load_model(model_path, task)
     collator = _load_collator(tokenizer, task)
-    args = _get_training_args()
+    args = _get_training_args(run_name, epochs, batch_size)
 
     trainer = Trainer(
         args=args,
