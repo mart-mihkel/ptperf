d339d9cc6013c96d4e51140cae26454aa927b350
mart-mihkel <mart.mihkel.aun@gmail.com>
2026-04-09 20:21:11 +0300

    fix sanity test params

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -10,29 +10,29 @@ _t5 = "hf-internal-testing/tiny-random-t5"
 
 def test_causal_fine_tune_sanity(gpt2_wikitext: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=gpt2_wikitext):
-        fine_tune(_gpt2, "causal-lm", "fine-tune", "test", 0, 1)
+        fine_tune(_gpt2, "causal-lm", "fine-tune", "test", 0, epochs=0)
 
 
 def test_causal_lora_sanity(gpt2_wikitext: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=gpt2_wikitext):
-        fine_tune(_gpt2, "causal-lm", "lora", "test", 0, 1)
+        fine_tune(_gpt2, "causal-lm", "lora", "test", 0, epochs=0)
 
 
 def test_causal_prefix_tune_sanity(gpt2_wikitext: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=gpt2_wikitext):
-        fine_tune(_gpt2, "causal-lm", "prefix-tune", "test", 0, 1)
+        fine_tune(_gpt2, "causal-lm", "prefix-tune", "test", 0, epochs=0)
 
 
 def test_seq_fine_tune_sanity(t5_samsum: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=t5_samsum):
-        fine_tune(_t5, "seq2seq", "fine-tune", "test", 0, 1)
+        fine_tune(_t5, "seq2seq", "fine-tune", "test", 0, epochs=0)
 
 
 def test_seq_lora_sanity(t5_samsum: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=t5_samsum):
-        fine_tune(_t5, "seq2seq", "lora", "test", 0, 1)
+        fine_tune(_t5, "seq2seq", "lora", "test", 0, epochs=0)
 
 
 def test_seq_prefix_tune_sanity(t5_samsum: DatasetDict):
     with patch("ptperf.datasets.load_data", return_value=t5_samsum):
-        fine_tune(_t5, "seq2seq", "prefix-tune", "test", 0, 1)
+        fine_tune(_t5, "seq2seq", "prefix-tune", "test", 0, epochs=0)
